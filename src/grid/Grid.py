3141b8fb304a2c796e4289c93986c56ba0af8d47
Ibrahim Mira <iam7123@nyu.edu>
2024-05-15 12:35:12 -0400

    meeting notes and updates

--- src/grid/Grid.py
+++ src/grid/Grid.py
@@ -1,23 +1,30 @@
 import numpy as np
 
-class Grid:
-
+class Domain:
+    '''
+    Coordinates Class
+       .x (meshgrid)
+       .y (meshgrid)
+       shape
+    '''
     def __init__(self, x_span: int, y_span: int, num_x_coords: int, num_y_coords: int) -> None:
         
-        # x - value
+        # x - span
         self.x_span = x_span
 
-        # y - value
+        # y - span
         self.y_span = y_span
 
-        # x - spacing
+        # x - num of coords
         self.num_x_coords = num_x_coords
         
-        # y - spacing
+        # y - num of coords
         self.num_y_coords = num_y_coords
+        
+        self.main_coords = self._domain_setup()
+        
 
-
-    def _grid_setup(self) -> tuple:
+    def _domain_setup(self) -> tuple:
         
         # Determine min & max values
         x_min, x_max = (self.x_span * -1) / 2, self.x_span / 2
@@ -27,18 +34,10 @@ class Grid:
         x_coords = np.linspace(x_min, x_max, self.num_x_coords)
         y_coords = np.linspace(y_min, y_max, self.num_y_coords)
 
-        return x_coords, y_coords
-
-
-    def make_grid(self) -> tuple:
-        
-        # Get x and y coordinates
-        x_coords, y_coords = self._grid_setup()
-
         # Generate grid using np.meshgrid
-        x_grid, y_grid = np.meshgrid(x_coords, y_coords)
+        return  Coordinates(np.meshgrid(x_coords, y_coords))
 
         # # Use np.vstack to help visualize grid points (Could be useful later)
         # grid_points = np.vstack([x_grid.ravel(), y_grid.ravel()]).T
 
-        return x_grid, y_grid
+        
--- tests/test_grid.py
+++ tests/test_grid.py
@@ -1,12 +1,12 @@
 # Testing Grid.py
 import pytest
 import numpy as np
-from src.Grid import Grid.py
+from src.grid.Grid import Grid
 
 # Generate Sample Grid
 def generate_sample_grid(x_span, y_span, num_x_coords, num_y_coords):
 
-    return Grid(7,7, 7, 7)
+    return Grid(7,7,7,7)
 
 # Test imports
 def test_imports():
@@ -21,7 +21,7 @@ def test_check_span():
     intended_x_span = grid.x_span
     intended_y_span = grid.y_span
 
-    # Unpacks the tupe of grids returned by make_grid()
+    # Unpacks the tuple of grids returned by make_grid()
     x_grid, y_grid = grid.make_grid()
    
     assert len(x_grid) == intended_x_span
@@ -35,7 +35,7 @@ def test_check_grid_size():
 
     intended_grid_size = (7, 7)
 
-    # Unpacks the tupe of grids returned by make_grid()
+    # Unpacks the tuple of grids returned by make_grid()
     x_grid, y_grid = grid.make_grid()
     x_size = x_grid.shape
     y_size = y_grid.shape
