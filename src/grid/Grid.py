343010904effef1630d03548ec4dd166f8addaa2
Ibrahim Mira <iam7123@nyu.edu>
2024-05-14 22:47:35 -0400

    fixed some issues with code after pull request comments

--- src/grid/Grid.py
+++ src/grid/Grid.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class Grid:
 
-    def __init__(self, x_span: int, y_span: int, x_spacing: int, num_y_points: int) -> None:
+    def __init__(self, x_span: int, y_span: int, num_x_coords: int, num_y_coords: int) -> None:
         
         # x - value
         self.x_span = x_span
@@ -10,26 +10,22 @@ class Grid:
         # y - value
         self.y_span = y_span
 
-        # x - scaling
-        self.x_spacing = x_spacing
+        # x - spacing
+        self.num_x_coords = num_x_coords
         
-        # y - scaling
-        self.y_spacing = num_y_points
+        # y - spacing
+        self.num_y_coords = num_y_coords
 
-        
+
     def _grid_setup(self) -> tuple:
         
         # Determine min & max values
         x_min, x_max = (self.x_span * -1) / 2, self.x_span / 2
         y_min, y_max = (self.y_span * -1) / 2, self.y_span / 2
 
-        # Calculate number of points based on spacing
-        num_x_coords = int(self.x_span / self.x_spacing) + 1
-        num_y_coords = int(self.y_span / self.y_spacing) + 1
-
         # Create x and y coordinates
-        x_coords = np.linspace(x_min, x_max, num_x_coords)
-        y_coords = np.linspace(y_min, y_max, num_y_coords)
+        x_coords = np.linspace(x_min, x_max, self.num_x_coords)
+        y_coords = np.linspace(y_min, y_max, self.num_y_coords)
 
         return x_coords, y_coords
 
