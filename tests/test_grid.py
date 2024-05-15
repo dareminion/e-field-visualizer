6d919b5cbc7a37daf06b294d31f5428d1f239c8d
Ibrahim Mira <iam7123@nyu.edu>
2024-05-14 23:55:50 -0400

    added check grid size test

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -26,3 +26,19 @@ def test_check_span():
    
     assert len(x_grid) == intended_x_span
     assert len(y_grid) == intended_y_span
+
+# Check if the grid size is correct:
+def test_check_grid_size():
+
+    # Generates a grid
+    grid = generate_sample_grid(7, 7, 7, 7)
+
+    intended_grid_size = (7, 7)
+
+    # Unpacks the tupe of grids returned by make_grid()
+    x_grid, y_grid = grid.make_grid()
+    x_size = x_grid.shape
+    y_size = y_grid.shape
+
+    assert intended_grid_size == x_size
+    assert intended_grid_size == y_size
