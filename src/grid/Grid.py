1ee4ce35d7f140d6d967e816c3e08c86edd5a34e
Ibrahim Mira <iam7123@nyu.edu>
2024-05-14 21:43:37 -0400

    created Grid Class and defined __init__

---
+++ src/grid/Grid.py
@@ -0,0 +1,18 @@
+class Grid:
+
+    def __init__(self, x_span: int, y_span: int, x_spacing: int, num_y_points: int) -> None:
+        
+        # x - value
+        self.x_span = x_span
+
+        # y - value
+        self.y_span = y_span
+
+        # x - scaling
+        self.x_spacing = x_spacing
+        
+        # y - scaling
+        self.y_spacing = num_y_points
+
+    def make_grid(self) -> None:
+        pass
