b14835f9078ed8ced10a8db0d31ffff0112b87d5
John <jf4707@nyu.edu>
2024-05-16 16:55:06 -0400

    POINT_CHARGE.py with efield and potential functions

--- src/physics/Point_Charge.py
+++ src/physics/Point_Charge.py
@@ -10,19 +10,34 @@ k_C = const.Coulomb
 
 class ElectrostaticPtCharge(FiducialElectrostaticSource):
 
-    def electric_field(self, coordinates: Coordinates) -> VectorField:
-        
+    def variables(coordinates: Coordinates) -> tuple:
+
         # x and y coordinates in numpy arrays
         x = coordinates.x_grid
         y = coordinates.y_grid
 
         r = ((x ** 2) + (y ** 2)) ** 1/2
 
+        return x,y,r
+
+    def electric_field(self, coordinates: Coordinates) -> VectorField:
+        
+        x,y,r = variables(coordinates)
+
         x_magnitude = (((k_c) * q) / r ** 2 ) * (x/r)
         y_magnitude = (((k_c) * q) / r ** 2 ) * (y/r)
 
         efield = np.dstack(x_magnitude, y_magnitude)
 
-        return efield
+        return VectorField(efield)
+
+    def epotential(self, coordinates: Coordinates) -> ScalarField:
+
+        x,y,r = variables(coordinates)
+
+        electric_potential = (k_C * q) / r
+
+        return ScalarField(electric_potential)
+
 
         
