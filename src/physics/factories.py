04b6454a837fa043cd2a6bf2d16f0a59852f3ba0
John <jf4707@nyu.edu>
2024-05-16 14:17:00 -0400

    Updates to factories.py

--- src/physics/factories.py
+++ src/physics/factories.py
@@ -14,3 +14,17 @@ class PlaceableSourceFactory():
         field_source = self.create_field_source()
         grid_transfromer = self.create_grid_transformer()
         return PlacealbeSource(field_source, gird_transformer)
+
+class PtSourcePlaceableFactory(PlaceableSourceFactory):
+    @staticmethod
+    def make_field_source(q = None):
+        return PointSource(q)
+
+
+    @staticmethod
+    def make_grid_transfromer():
+        shifter = gridtransforms.gridshift
+        def gtransfroms(g placement_data):
+            shift_data = placement_data['shift']
+            return shifter(g, shift_data)
+        return gtransforms
