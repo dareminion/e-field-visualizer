a32021cc4977b680ea469cb5383c97b27b7bfaa9
John <jf4707@nyu.edu>
2024-05-16 00:47:49 -0400

    First draft of the placesource class

--- src/physics/Point_Charge.py
+++ src/physics/Point_Charge.py
@@ -1,16 +1,27 @@
-class PlaceableSourceFactory():
-    @staticmethod
-    def create_field_source():
+import abc
+
+class PlaceableSource(abc.ABC):
+
+    placement_data = None
+    transformed_grid = None
+
+    def __init__(self, fiedsource, gridtransformer):
+        self.fieldsource = fieldsource
+        self.gridtransformer = gridtransformer
+
+    
+    def _gridtransform(self, grid):
         pass
 
-    #another method here
-    @abstractmethod
-    def create_grid_transformer():
-        pass
 
-    @abstractmethod
-    @staticmethod
-    def create():
-        field_source = self.create_field_source()
-        grid_transfromer = self.create_grid_transformer()
-        return PlacealbeSource(field_source, gird_transformer)
+    def place(self, placement_date: dict):
+        self.placement_data = placement_data
+        self._transformed_grid = None
+
+    def get_vector_field(grid):
+        tgrid = self._gridtransform(grid)
+        return self.fieldsource.get_vector_field(tgrid)
+
+    def get_scalar_field(grid):
+        tgrid = self._gridtransform(grid)
+        return self.fieldsource.get_scalar_field(self._transformed_grid)
