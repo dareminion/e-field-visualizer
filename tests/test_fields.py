fec4d650a5c38319aded40b6badee605a0fc933a
Darren <dk4461@nyu.edu>
2024-05-16 00:52:23 -0400

    Ending work for the night

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -31,18 +31,33 @@ def test_VectorField_Wrapper_Test():
     assert test_vector_field._field.all() == Expected_Array.all()
 
 def test_Scalar_Addition():
-    Expected_Array = np.full((25,25), 10)
-    Scalar_Numpy_Array_1 = np.full((25,25), 4)
-    Scalar_Numpy_Array_2 = np.full((25,25), 6)
+    Expected_Array = np.full((2,2), 10)
+    Scalar_Numpy_Array_1 = np.full((2,2), 4)
+    Scalar_Numpy_Array_2 = np.full((2,2), 6)
     test_scalar_field_1 = ScalarField(Scalar_Numpy_Array_1)
     test_scalar_field_2 = ScalarField(Scalar_Numpy_Array_2)
-    assert Expected_Array.all() == (test_scalar_field_1 + test_scalar_field_2)._field.all()
+    assert np.array_equal(Expected_Array, (test_scalar_field_1 + test_scalar_field_2)._field)
 
 def test_Scalar_Addition_Error_Case():
     Expected_Array = np.full((25,25), 4)
     Scalar_Numpy_Array_1 = np.full((25,25), 4)
-    Scalar_Numpy_Array_2 =  np.full((5,5), 6)
+    Scalar_Numpy_Array_2 = np.full((5,5), 6)
     test_scalar_field_1 = ScalarField(Scalar_Numpy_Array_1)
     test_scalar_field_2 = ScalarField(Scalar_Numpy_Array_2)
-    assert Expected_Array.all() == (test_scalar_field_1 + test_scalar_field_2)._field.all()
+    assert np.array_equal(Expected_Array, (test_scalar_field_1 + test_scalar_field_2)._field)
 
+def test_Vector_Addition():
+    Expected_Array = np.full((20,30,2),(100,50))
+    Vector_Numpy_Array_1 = np.full((20,30,2),(73,19))
+    Vector_Numpy_Array_2 = np.full((20,30,2),(27,31))
+    test_vfield1 = VectorField(Vector_Numpy_Array_1)
+    test_vfield2 = VectorField(Vector_Numpy_Array_2)
+    assert np.array_equal(Expected_Array, (test_vfield1 + test_vfield2)._field)
+
+def test_Vector_Addition_Error_Case():
+    Expected_Array = np.full((20,30,2),(100,50))
+    Vector_Numpy_Array_1 = np.full((20,30,2),(73,19))
+    Vector_Numpy_Array_2 = np.full((20,30,40,3),(27,31,10))
+    test_vfield1 = VectorField(Vector_Numpy_Array_1)
+    test_vfield2 = VectorField(Vector_Numpy_Array_2)
+    assert np.array_equal(Expected_Array, (test_vfield1 + test_vfield2)._field)
