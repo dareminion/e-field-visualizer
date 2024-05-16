c706df8d16aeeb110d9d664c113bfca994f31e95
ibrahimmira <135983387+ibrahimmira@users.noreply.github.com>
2024-05-16 18:45:18 -0400

    fixed some errors

--- src/visuals/Visualizer.py
+++ src/visuals/Visualizer.py
@@ -6,17 +6,23 @@ class Visualizer:
 
     # Creates a default plot of a pre-determined size that can be changed later
     def __init__(self, figsize: tuple=(10,10)) -> None:
+        
+        # self.figure is a new figure that gets generated at each instantiation
+        # self.axes are a pair of axes (subplots) that get generated at each instantiation
         self.figure, self.axes = plt.subplots(figsize=figsize)
 
     # Plot a vector field with arrows
     def plot_vector_field(self, vector_field, label='Vector Field', color='r'):
 
+        # Determine the domain size
+        n = int(np.sqrt(vector_field.shape[0]))
+
         # Create a meshgrid for the vector field
-        X, Y = np.meshgrid(np.arange(vector_field.shape[1]), np.arange(vector_field.shape[0]))
+        X, Y = np.meshgrid(np.arange(n), np.arange(n))
 
         # Extract the x and y components of the vector field
-        Ex = vector_field[:, :, 0]
-        Ey = vector_field[:, :, 1]
+        Ex = vector_field[:, 0].reshape(n, n)
+        Ey = vector_field[:, 1].reshape(n, n)
 
         # Plot the vector field using quiver
         self.axes.quiver(X, Y, Ex, Ey, color=color, label=label)
@@ -42,3 +48,5 @@ class Visualizer:
     # Display the plot
     def show(self):
         plt.show()
+
+
