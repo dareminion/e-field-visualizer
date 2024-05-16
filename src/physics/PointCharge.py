ee27e37582be0b4cdcc2e42c6da5dcc8db6813fc
John <jf4707@nyu.edu>
2024-05-16 18:37:23 -0400

    Modified ElectrostaticSource and PointCharge with pytests

--- src/physics/PointCharge.py
+++ src/physics/PointCharge.py
@@ -6,11 +6,11 @@ from src.physics.ElectrostaticSource import FiducialElectrostaticSource
 import scipy.constants as const
 import numpy as np
 
-k_C = const.Coulomb
+k_C = 8.99 * (10 ** 9)
 
 class ElectrostaticPtCharge(FiducialElectrostaticSource):
 
-    def variables(coordinates: Coordinates) -> tuple:
+    def variables(self, coordinates: Coordinates) -> tuple:
 
         # x and y coordinates in numpy arrays
         x = coordinates.x_grid
@@ -18,22 +18,24 @@ class ElectrostaticPtCharge(FiducialElectrostaticSource):
 
         r = ((x ** 2) + (y ** 2)) ** 1/2
 
-        return x,y,r
+        q = self.charge
+
+        return x,y,r,q
 
     def electric_field(self, coordinates: Coordinates) -> VectorField:
         
-        x,y,r = variables(coordinates)
+        x,y,r,q = self.variables(coordinates)
 
-        x_magnitude = (((k_c) * q) / r ** 2 ) * (x/r)
-        y_magnitude = (((k_c) * q) / r ** 2 ) * (y/r)
+        x_magnitude = (((k_C) * q) / r ** 2 ) * (x/r)
+        y_magnitude = (((k_C) * q) / r ** 2 ) * (y/r)
 
-        efield = np.dstack(x_magnitude, y_magnitude)
+        efield = np.dstack((x_magnitude, y_magnitude))
 
         return VectorField(efield)
 
-    def epotential(self, coordinates: Coordinates) -> ScalarField:
+    def electric_potential(self, coordinates: Coordinates) -> ScalarField:
 
-        x,y,r = variables(coordinates)
+        x,y,r,q = self.variables(coordinates)
 
         electric_potential = (k_C * q) / r
 
