cdd11e3091a76a92fecfce14dd9fd14559ed76f5
John <jf4707@nyu.edu>
2024-05-16 14:16:35 -0400

    Updates to POINT_CHARGE.py

--- src/physics/Point_Charge.py
+++ src/physics/Point_Charge.py
@@ -7,14 +7,16 @@ class PlaceableSource(abc.ABC):
 
     def __init__(self, fiedsource, gridtransformer):
         self.fieldsource = fieldsource
-        self.gridtransformer = gridtransformer
+        self._gridtransformer = gridtransformer
 
     
     def _gridtransform(self, grid):
-        pass
+        if not self._transformed_grid:
+            self._transformed_grid = self._gridtransformer(g,self.placement_data)
+            return self._transfromed_grid
 
 
-    def place(self, placement_date: dict):
+    def place(self, placement_data: dict):
         self.placement_data = placement_data
         self._transformed_grid = None
 
@@ -25,3 +27,5 @@ class PlaceableSource(abc.ABC):
     def get_scalar_field(grid):
         tgrid = self._gridtransform(grid)
         return self.fieldsource.get_scalar_field(self._transformed_grid)
+
+    
