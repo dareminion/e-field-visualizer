bc0bcef37c3e7ca6812bb8c9c7b9f00762075503
ibrahimmira <135983387+ibrahimmira@users.noreply.github.com>
2024-05-16 16:06:51 -0400

    added tests for get_efield and get_epotential

--- tests/test_coordinates.py
+++ tests/test_coordinates.py
@@ -3,6 +3,9 @@ import pytest
 import numpy as np
 from src.domain.Domain import Domain
 from src.domain.Coordinates import Coordinates
+from src.physics.placeablesource import PlaceableSource
+from src.math.VectorFields import VectorField
+from src.math.ScalarFields import ScalarField
 
 # Generate Sample Coordinate object
 def generate_sample_coordinate():
@@ -13,6 +16,7 @@ def generate_sample_coordinate():
 def test_imports() -> None:
     assert True
 
+# Tests if coordinates are shifted correctly
 def test_shift_coordinates() -> None:
 
     original_coords = generate_sample_coordinate()
@@ -20,3 +24,21 @@ def test_shift_coordinates() -> None:
 
     assert np.array_equal(shifted_coords.x_grid , original_coords.x_grid + 3)
     assert np.array_equal(shifted_coords.y_grid , original_coords.y_grid + 3)
+
+# Tests return type of get_efield()
+def test_get_efield_return_type() -> None:
+
+    sample_domain = Domain(3, 3, 3, 3)
+    sample_coords = Domain(3, 3, 3, 3).main_coords
+    source = PlaceableSource()
+    result = domain.get_efield(source, sample_coords)
+    assert isinstance(result, VectorField)
+
+# Tests return type of get_epotential()
+def test_get_epotential_return_type() -> None:
+
+    sample_domain = Domain(3, 3, 3, 3)
+    sample_coords = Domain(3, 3, 3, 3).main_coords
+    source = PlaceableSource()
+    result = domain.get_epotential(source, sample_coords)
+    assert isinstance(result, ScalarField)
