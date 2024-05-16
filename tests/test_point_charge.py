bba46643cf54abf7cdc0a323a2a601b1199f176a
John <jf4707@nyu.edu>
2024-05-16 17:07:48 -0400

    test_point_charge.py testing imports

---
+++ tests/test_point_charge.py
@@ -0,0 +1,8 @@
+import pytest
+from src.math.Fields import Field
+from src.math.ScalarFields import ScalarField
+from src.math.VectorFields import VectorField
+import numpy as np
+
+def test_import():
+    assert True
