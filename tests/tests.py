26364cf486bd72c9358c4269ad022cba221be2e4
Darren <dk4461@nyu.edu>
2024-05-15 20:01:51 -0400

    Updated tests.py for workflow

---
+++ tests/tests.py
@@ -0,0 +1,17 @@
+'''
+Once class files are created and PR'd into main repo, more imports will be defined
+'''
+
+import pytest
+import numpy as np
+
+
+'''
+test_workflows is just a temporary test for the workflows test to pass when a PR occurs
+'''
+def test_workflows():
+    assert True
+
+'''
+Small Integration Tests will be here after class creatio is complete
+'''
