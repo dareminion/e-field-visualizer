091e49464561254247ee02d5a60653798df12de9
ibrahimmira <135983387+ibrahimmira@users.noreply.github.com>
2024-05-16 17:25:31 -0400

    wrote initial code for plotting vector_field

--- src/visuals/Visualizer.py
+++ src/visuals/Visualizer.py
@@ -2,8 +2,34 @@ import numpy as np
 import matplotlib as plt
 from src.domain.Domain import Domain
 
-class Visulizer:
+class Visualizer:
 
-    def __init__() -> None:
+    # Creates a default plot of a pre-determined size that can be changed later
+    def __init__(self, figsize: tuple=(10,10)) -> None:
+        self.figure, self.axes = plt.subplots(figsize=figsize)
+
+    # Plot a vector field with arrows
+    def plot_vector_field(self, vector_field, label='Vector Field', color='r'):
+
+        # Create a meshgrid for the vector field
+        X, Y = np.meshgrid(np.arange(vector_field.shape[1]), np.arange(vector_field.shape[0]))
+
+        # Extract the x and y components of the vector field
+        Ex = vector_field[:, :, 0]
+        Ey = vector_field[:, :, 1]
+
+        # Plot the vector field using quiver
+        self.ax.quiver(X, Y, Ex, Ey, color=color, label=label)
+        self.ax.set_title('Vector Field')
+        self.ax.set_xlabel('x')
+        self.ax.set_ylabel('y')
+        self.ax.grid()
+
+        if label:
+            self.ax.legend()
+    
+    # Plot a scalar field as a heatmap
+    def plot_scalar_field(self, scalar_field, label='Electric Potential', cmap='viridis'):
         pass
+
     
