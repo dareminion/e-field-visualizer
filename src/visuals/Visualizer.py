be384ec44f6233488a584b4f4c410e09c0cb38bb
ibrahimmira <135983387+ibrahimmira@users.noreply.github.com>
2024-05-16 17:40:17 -0400

    fixed some typos

--- src/visuals/Visualizer.py
+++ src/visuals/Visualizer.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from src.domain.Domain import Domain
 
 class Visualizer:
@@ -19,28 +19,26 @@ class Visualizer:
         Ey = vector_field[:, :, 1]
 
         # Plot the vector field using quiver
-        self.ax.quiver(X, Y, Ex, Ey, color=color, label=label)
-        self.ax.set_title('Vector Field')
-        self.ax.set_xlabel('x')
-        self.ax.set_ylabel('y')
-        self.ax.grid()
+        self.axes.quiver(X, Y, Ex, Ey, color=color, label=label)
+        self.axes.set_title('Vector Field')
+        self.axes.set_xlabel('x')
+        self.axes.set_ylabel('y')
+        self.axes.grid()
 
         if label:
-            self.ax.legend()
+            self.axes.legend()
     
     # Plot a scalar field as a heatmap
     def plot_scalar_field(self, scalar_field, label='Electric Potential', cmap='viridis'):
 
         # Plot the scalar field using imshow
-        heatmap = self.ax.imshow(scalar_field, extent=(0, scalar_field.shape[1], 0, scalar_field.shape[0]), origin='lower', cmap=cmap, alpha=0.6)
+        heatmap = self.axes.imshow(scalar_field, extent=(0, scalar_field.shape[1], 0, scalar_field.shape[0]), origin='lower', cmap=cmap, alpha=0.6)
         
         # Add a color bar to the heatmap
-        cbar = self.fig.colorbar(heatmap, ax=self.ax, label=label)
+        cbar = self.figure.colorbar(heatmap, ax=self.axes, label=label)
         cbar.set_alpha(1)
-        cbar.draw_all()
+        cbar._draw_all()
 
     # Display the plot
     def show(self):
         plt.show()
-
-    
