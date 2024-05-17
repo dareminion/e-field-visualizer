5aa341da14403a9080df8e8ebf2dd7b5eb2ab7b1
Darren <dk4461@nyu.edu>
2024-05-17 08:59:01 -0400

    VectorFieldSource Protocol Created

---
+++ src/math/VectorFieldSource.py
@@ -0,0 +1,12 @@
+import abc
+import numpy as np
+from src.math.VectorFields import VectorField
+
+class VectorFieldSource(abc.ABC):
+    '''
+    Protocol to ensure Vector Field generation from a source
+    '''
+
+    @abc.abstractmethod
+    def get_vector_field(x_coords: np.ndarray, y_coords: np.ndarray) -> VectorField:
+        pass
