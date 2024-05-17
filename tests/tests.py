95abb38eada6a2a47994997e656396f9bd6a0463
Darren <dk4461@nyu.edu>
2024-05-17 17:33:09 -0400

    wrote partial end to end integration test

--- tests/tests.py
+++ tests/tests.py
@@ -4,7 +4,26 @@ Once class files are created and PR'd into main repo, more imports will be defin
 
 import pytest
 import numpy as np
+from src.math.VectorFields import VectorField
+from src.factories.PointSourceFactory import PointSourceFactory
+from src.physics.PlaceableSource import PlaceableSource
+from src.domain.Domain import Domain
 
+def test_FactorytoVectorField():
+    domain = Domain(10, 10, 10, 10)
+
+    placement_data = {'x' : 2,
+            'y' : 3}
+
+    factory = PointSourceFactory()
+
+    source = factory.create_placeable_source(10)
+
+    source.place(placement_data)
+
+    vector_field = source.get_vector_field(domain.main_coords)
+
+    assert isinstance(vector_field, VectorField)
 
 '''
 test_workflows is just a temporary test for the workflows test to pass when a PR occurs
